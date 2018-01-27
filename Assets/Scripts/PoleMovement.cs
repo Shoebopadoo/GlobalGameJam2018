fd4b34ec9c9523a6801a4091ea6c34e98ba2249e
killswitchq <bsotherden@gmail.com>
2018-01-27 01:30:39 -0500

    Strings b workin

---
+++ Assets/Scripts/PoleMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleMovement : MonoBehaviour {
+    public GameObject Object1;
+    public GameObject Object2;
+   
+	// Use this for initialization
+	void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<LineRenderer>().SetPosition(0, Object1.transform.position);
+        GetComponent<LineRenderer>().SetPosition(1, Object2.transform.position);
+    }
+}
