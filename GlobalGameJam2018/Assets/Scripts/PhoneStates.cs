1d7d06b0a0f0618e411ec1f523978ccff984937d
bnelligan <nelligan81@gmail.com>
2018-01-27 12:46:12 -0500

    State machine start
    Started Phoneline state machine

--- GlobalGameJam2018/Assets/Scripts/PhoneStates.cs
+++ GlobalGameJam2018/Assets/Scripts/PhoneStates.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Reflection;
+using System.Text;
 using UnityEngine;
 
-public abstract class PhoneState  {
 
+public abstract class PhoneState  {
+    
     public abstract void OnEnter(PhoneLine line);
     public abstract void OnUpdate(PhoneLine line);
     public abstract void OnExit(PhoneLine line);
 
-    
+    public static void ChangeState<T>(PhoneLine line, PhoneState state) where T : PhoneState
+    {
+        state.OnExit(line);
+        state = Activator.CreateInstance<T>();
+        state.OnEnter(line);
+    }
 }
