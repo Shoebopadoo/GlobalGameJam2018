26f8df1e9cc42557beef2bafaec66b0469c2f801
Aaron Benjamin <lifeartstudios@gmail.com>
2018-01-28 15:04:07 -0500

    Added Jack numbering
    Updated jack complete prefab

---
+++ Assets/Scripts/NumberAssign.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NumberAssign : MonoBehaviour
+{
+    public Text myTextobj;
+    public int myID;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myID = gameObject.GetComponent<Jack>().Id;
+        myTextobj = gameObject.GetComponentInChildren<Text>();
+        myTextobj.text = myID.ToString();
+	}
+}
