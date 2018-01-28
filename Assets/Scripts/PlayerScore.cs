261c84b72f6dc407f2d4e83c1b33eabfef4a7ca5
bnelligan <nelligan81@gmail.com>
2018-01-28 00:52:13 -0500

    PlayerScore

---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScore : MonoBehaviour {
+
+    private float _totalScore = 0f;
+    private float _multiplier = 1f;
+    private float _dMult = .2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddScore(float score)
+    {
+        _totalScore += score;
+    }
+
+    private void UpdateScore()
+    {
+
+    }
+
+
+}
