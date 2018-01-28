823c2a2cf554a8cf075ee83e590854ae5cbfe865
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-01-28 12:19:44 -0500

    light flicker
    added flickering light script

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    Light _light;
+	// Use this for initialization
+	void Start () {
+        _light = GetComponent<Light>();
+        StartCoroutine(Flash());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator Flash()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+            _light.enabled = !_light.enabled;
+        }
+    }
+}
