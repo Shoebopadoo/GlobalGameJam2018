5715d07e03c20593164ad6ee370b91ec4dec2ad1
bnelligan <nelligan81@gmail.com>
2018-01-28 15:30:14 -0500

    Life Indicator

---
+++ Assets/LifeTracker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTracker : MonoBehaviour {
+
+    [SerializeField]
+    Light _first;
+    [SerializeField]
+    Light _second;
+    [SerializeField]
+    Light _third;
+    
+	
+    public void LoseLife()
+    {
+        if(_third.enabled == true)
+        {
+            _third.color = Color.red;
+        }
+        else if (_second.enabled == true)
+        {
+            _second.color = Color.red;
+        }
+        else if (_first.enabled == true)
+        {
+            _first.color = Color.red;
+        }
+    }
+}
